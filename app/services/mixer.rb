788878b34d51ae9e30a80edd7b3d81a20c3f6ece
James Kiesel <james.kiesel@gmail.com>
2018-08-25 16:31:32 -0500

    Add group size setting

--- app/services/mixer.rb
+++ app/services/mixer.rb
@@ -1,12 +1,18 @@
 module Mingle
   Mixer = Struct.new(:group) do
     def mix!
-      a_users, b_users = pairs
-      a_users.zip(b_users)
+      a_users, *b_users = pairs
+      a_users.zip(*b_users)
     end
 
     def pairs
-      @pairs ||= group.users.real.not_suspended.activated.order("RANDOM()").in_groups(2)
+      @pairs ||= group
+        .users
+        .real
+        .not_suspended
+        .activated
+        .order("RANDOM()")
+        .in_groups(SiteSetting.mingle_group_size)
     end
   end
 end
