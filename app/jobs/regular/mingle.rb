376c2a653e2741e44d2ae28f4e7ba6be17b905ad
James Kiesel <james.kiesel@gmail.com>
2018-08-26 13:34:12 -0500

    Allow for multiple group names

--- app/jobs/regular/mingle.rb
+++ app/jobs/regular/mingle.rb
@@ -4,24 +4,24 @@ module Jobs
 
     def execute(args = {})
       return if !SiteSetting.mingle_enabled
-      return log_mismingle unless mingle_pairs.presence
-      mingle_pairs.each { |pair| ::Mingle::Sender.new(pair.compact).send! }
+      return log_mismingle unless mingle_sets.presence
+      mingle_sets.each { |pair| ::Mingle::Sender.new(pair.compact).send! }
     ensure
       ::Mingle::Scheduler.new.reschedule!
     end
 
     private
 
-    def mingle_pairs
-      @mingle_pairs ||= ::Mingle::Mixer.new(mingle_group).mix!
+    def mingle_sets
+      @mingle_sets ||= ::Mingle::Mixer.new(mingle_users).mix!
     end
 
-    def mingle_group
-      @mingle_group ||= ::Group.find_by(name: SiteSetting.mingle_group_name) || Group.new
+    def mingle_users
+      @mingle_users ||= User.distinct.joins(:groups).where("groups.name": SiteSetting.mingle_group_name.split('|'))
     end
 
     def log_mismingle
-      Rails.logger.warn "No pairs generated from Mingle, group name was '#{SiteSetting.mingle_group_name}'. Are you sure that group exists and has members in it?" if mingle_group.users.empty?
+      Rails.logger.warn "No pairs generated from Mingle, group name(s) were '#{SiteSetting.mingle_group_name}'. Are you sure that at least one group exists and has members in it?" if mingle_users.empty?
     end
   end
 end
--- app/services/mixer.rb
+++ app/services/mixer.rb
@@ -1,18 +1,13 @@
 module Mingle
-  Mixer = Struct.new(:group) do
+  Mixer = Struct.new(:users) do
     def mix!
-      a_users, *b_users = pairs
-      a_users.zip(*b_users)
-    end
-
-    def pairs
-      @pairs ||= group
-        .users
+      a_users, *b_users = users
         .real
         .not_suspended
         .activated
         .order("RANDOM()")
         .in_groups(SiteSetting.mingle_group_size)
+      a_users.zip(*b_users)
     end
   end
 end
